47c939f4fea9dbbba672db6ac632277dac30073e
Adarsh.M <adarshmelath1305@gmail.com>
2026-05-19 11:34:07 +0530

    chore(tests): disable vault to prevent CI error

--- backend/src/test/java/com/adarsh/backend/BackendApplicationTests.java
+++ backend/src/test/java/com/adarsh/backend/BackendApplicationTests.java
@@ -3,7 +3,9 @@ package com.adarsh.backend;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(properties={
+	"spring.cloud.vault.enabled=false"
+})
 class BackendApplicationTests {
 
 	@Test
