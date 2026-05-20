c61ed30cfcae71d66fd3c988b326390a485e22cf
Adarsh.M <adarshmelath1305@gmail.com>
2026-05-20 21:58:53 +0530

    feat(user-usecase): update return type of method

--- backend/src/main/java/com/adarsh/backend/feature/user/application/usecase/CreateUserUseCase.java
+++ backend/src/main/java/com/adarsh/backend/feature/user/application/usecase/CreateUserUseCase.java
@@ -1,8 +1,7 @@
 package com.adarsh.backend.feature.user.application.usecase;
 
 import com.adarsh.backend.feature.user.application.dto.CreateUserCommand;
-import com.adarsh.backend.feature.user.domain.model.User;
 
 public interface CreateUserUseCase {
-    User execute(CreateUserCommand command);
+    void execute(CreateUserCommand command);
 }
