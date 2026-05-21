4f06050d11c1dcd7699cf3a7d878a8c0a4ba6e40
Adarsh.M <adarshmelath1305@gmail.com>
2026-05-21 10:24:12 +0530

    feat(emailservice): add otp mail service

---
+++ backend/src/main/java/com/adarsh/backend/shared/infrastructure/email/EmailService.java
@@ -0,0 +1,24 @@
+package com.adarsh.backend.shared.infrastructure.email;
+
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+import com.adarsh.backend.shared.application.SendOtpUseCase;
+
+import lombok.RequiredArgsConstructor;
+
+
+@Service
+@RequiredArgsConstructor
+public class EmailService implements SendOtpUseCase {
+    private final JavaMailSender mailSender;
+
+    public void sendOtp(String toEmail, String otp) {
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setTo(toEmail);
+        message.setSubject("The Bibliophile's Bazar -  Email verification");
+        message.setText("Your OTP for email verification is: " + otp);
+        mailSender.send(message);
+    }
+}
