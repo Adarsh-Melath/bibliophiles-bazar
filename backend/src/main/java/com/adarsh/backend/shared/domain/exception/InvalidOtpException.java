3f2e3c7ae668ddb230bd91372098d6bafaf3c974
Adarsh.M <adarshmelath1305@gmail.com>
2026-05-21 14:45:27 +0530

    feat(otp): add invalid otp exception

---
+++ backend/src/main/java/com/adarsh/backend/shared/domain/exception/InvalidOtpException.java
@@ -0,0 +1,7 @@
+package com.adarsh.backend.shared.domain.exception;
+
+public class InvalidOtpException extends RuntimeException {
+    public InvalidOtpException(String message) {
+        super(message);
+    }
+}
