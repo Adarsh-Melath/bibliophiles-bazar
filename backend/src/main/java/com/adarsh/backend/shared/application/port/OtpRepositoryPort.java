911364d636cd8fbbc17b20dfd9c55182e720181f
Adarsh.M <adarshmelath1305@gmail.com>
2026-05-22 11:25:59 +0530

    refactor(otp): change return type of getLatestOtpByEmail

--- backend/src/main/java/com/adarsh/backend/shared/application/port/OtpRepositoryPort.java
+++ backend/src/main/java/com/adarsh/backend/shared/application/port/OtpRepositoryPort.java
@@ -1,9 +1,13 @@
 package com.adarsh.backend.shared.application.port;
 
+import java.util.Optional;
+
+import com.adarsh.backend.feature.auth.domain.OtpToken;
+
 public interface OtpRepositoryPort {
     void saveOtp(String email, String otp);
 
-    String getLatestOtpByEmail(String email);
+    Optional<OtpToken> getLatestOtpByEmail(String email);
 
     void deleteOtpByEmail(String email);
 }
