427aa64fb99ac7d1009bb5495fc4b9f9c9de1e14
Adarsh.M <adarshmelath1305@gmail.com>
2026-05-22 15:01:09 +0530

    feat(exception): add InvalidCredentialException

---
+++ backend/src/main/java/com/adarsh/backend/feature/user/domain/exception/InvalidCredentialException.java
@@ -0,0 +1,7 @@
+package com.adarsh.backend.feature.user.domain.exception;
+
+public class InvalidCredentialException extends RuntimeException {
+    public InvalidCredentialException(String message) {
+        super(message);
+    }
+}
