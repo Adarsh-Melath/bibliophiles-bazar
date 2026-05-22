8d483b77d63bb387201af63eb427196f946cc565
Adarsh.M <adarshmelath1305@gmail.com>
2026-05-22 11:20:14 +0530

    refactor(otp): change method name to getLatestOtpByEmail

--- backend/src/main/java/com/adarsh/backend/shared/application/port/OtpRepositoryPort.java
+++ backend/src/main/java/com/adarsh/backend/shared/application/port/OtpRepositoryPort.java
@@ -3,7 +3,7 @@ package com.adarsh.backend.shared.application.port;
 public interface OtpRepositoryPort {
     void saveOtp(String email, String otp);
 
-    String getOtpByEmail(String email);
+    String getLatestOtpByEmail(String email);
 
     void deleteOtpByEmail(String email);
 }
