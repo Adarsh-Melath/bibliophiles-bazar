ab6af8b6fd532659df109d7a2bff33e1579999ca
Adarsh.M <adarshmelath1305@gmail.com>
2026-05-22 11:50:08 +0530

    feat(otp-exception): add OtpExpiredException handler

--- backend/src/main/java/com/adarsh/backend/shared/infrastructure/web/exception/GlobalExceptionHandler.java
+++ backend/src/main/java/com/adarsh/backend/shared/infrastructure/web/exception/GlobalExceptionHandler.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import com.adarsh.backend.shared.domain.exception.InvalidOtpException;
+import com.adarsh.backend.shared.domain.exception.OtpExpiredException;
 import com.adarsh.backend.shared.domain.exception.OtpNotFoundException;
 
 import jakarta.servlet.http.HttpServletRequest;
@@ -15,15 +16,27 @@ import jakarta.servlet.http.HttpServletRequest;
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
+    @ExceptionHandler(OtpExpiredException.class)
+    public ResponseEntity<ErrorResponse> handleOtpNotFoundException(OtpExpiredException ex,
+            HttpServletRequest request) {
+        ErrorResponse errorResponse = new ErrorResponse(
+                LocalDateTime.now(),
+                HttpStatus.GONE.value(),
+                "Expired",
+                ex.getMessage(),
+                request.getRequestURI());
+        return ResponseEntity.status(HttpStatus.GONE).body(errorResponse);
+    }
+
     @ExceptionHandler(OtpNotFoundException.class)
-    public ResponseEntity<ErrorResponse> handleOtpNotFoundException(OtpNotFoundException ex, HttpServletRequest request) {
+    public ResponseEntity<ErrorResponse> handleOtpNotFoundException(OtpNotFoundException ex,
+            HttpServletRequest request) {
         ErrorResponse errorResponse = new ErrorResponse(
                 LocalDateTime.now(),
                 HttpStatus.NOT_FOUND.value(),
                 "Not Found",
                 ex.getMessage(),
-                request.getRequestURI()
-        );
+                request.getRequestURI());
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(errorResponse);
     }
 
@@ -34,8 +47,7 @@ public class GlobalExceptionHandler {
                 HttpStatus.BAD_REQUEST.value(),
                 "Bad Request",
                 ex.getMessage(),
-                request.getRequestURI()
-        );
+                request.getRequestURI());
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorResponse);
     }
 
