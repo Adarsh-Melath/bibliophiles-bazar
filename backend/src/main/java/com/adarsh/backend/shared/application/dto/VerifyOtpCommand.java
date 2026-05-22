713a472576589c1868461c435b2a087282e33f73
Adarsh.M <adarshmelath1305@gmail.com>
2026-05-22 11:10:47 +0530

    feat(otp): add VerifyOtpCommand dto

---
+++ backend/src/main/java/com/adarsh/backend/shared/application/dto/VerifyOtpCommand.java
@@ -0,0 +1,25 @@
+package com.adarsh.backend.shared.application.dto;
+
+import java.util.Objects;
+
+public class VerifyOtpCommand {
+    private String email;
+    private String code;
+
+    public VerifyOtpCommand(String email, String code) {
+        Objects.requireNonNull(email, "Email cannot be null");
+        Objects.requireNonNull(code, "Otp cannot be null");
+
+        if (!email.contains("@")) {
+            throw new IllegalArgumentException("Invalid email format");
+        }
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getCode() {
+        return code;
+    }
+}
