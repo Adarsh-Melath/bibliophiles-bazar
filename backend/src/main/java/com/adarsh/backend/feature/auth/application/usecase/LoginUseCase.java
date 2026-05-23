d15661083621667defc27b587d3dc4e6661e3e6a
Adarsh.M <adarshmelath1305@gmail.com>
2026-05-23 15:56:41 +0530

    refactor(auth): change method return type in LoginUseCase

--- backend/src/main/java/com/adarsh/backend/feature/auth/application/usecase/LoginUseCase.java
+++ backend/src/main/java/com/adarsh/backend/feature/auth/application/usecase/LoginUseCase.java
@@ -1,7 +1,8 @@
 package com.adarsh.backend.feature.auth.application.usecase;
 
 import com.adarsh.backend.feature.auth.application.dto.LoginCommand;
+import com.adarsh.backend.feature.auth.application.dto.LoginResult;
 
 public interface LoginUseCase {
-    void execute(LoginCommand command);
+    LoginResult execute(LoginCommand command);
 }
