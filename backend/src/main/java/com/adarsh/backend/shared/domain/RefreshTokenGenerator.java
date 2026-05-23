8bc366fcea1583b76d9835f43165a821b8f3738e
Adarsh.M <adarshmelath1305@gmail.com>
2026-05-23 13:06:02 +0530

    feat(security): add RefreshTokenGenerator

---
+++ backend/src/main/java/com/adarsh/backend/shared/domain/RefreshTokenGenerator.java
@@ -0,0 +1,14 @@
+package com.adarsh.backend.shared.domain;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+public class RefreshTokenGenerator {
+    private RefreshTokenGenerator() {
+    }
+
+    public static RefreshToken generateRefreshToken(String email) {
+        return new RefreshToken.Builder().token(UUID.randomUUID().toString()).email(email)
+                .expiresAt(LocalDateTime.now().plusDays(7)).build();
+    }
+}
