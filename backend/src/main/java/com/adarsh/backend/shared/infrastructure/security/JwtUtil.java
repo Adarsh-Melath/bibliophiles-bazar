905b23f28cfd4007a2a18aa8b0dc8b61745f70a8
Adarsh.M <adarshmelath1305@gmail.com>
2026-05-23 12:00:58 +0530

    feat(security): implement JwtUtilPort

---
+++ backend/src/main/java/com/adarsh/backend/shared/infrastructure/security/JwtUtil.java
@@ -0,0 +1,45 @@
+package com.adarsh.backend.shared.infrastructure.security;
+
+import java.util.Date;
+
+import com.adarsh.backend.shared.application.port.JwtUtilPort;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.JwtException;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+
+public class JwtUtil implements JwtUtilPort {
+    private String jwtSecret;
+
+    private long expiration;
+
+    @Override
+    public String generateAccessToken(String email, String role) {
+        long currentTimeMillis = System.currentTimeMillis();
+
+        long expirationTime = currentTimeMillis + expiration;
+        return Jwts.builder()
+                .subject(email)
+                .claim("role", role)
+                .issuedAt(new Date()).expiration(new Date(expirationTime))
+                .signWith(Keys.hmacShaKeyFor(jwtSecret.getBytes()))
+                .compact();
+    }
+
+    @Override
+    public Claims extractClaims(String token) {
+        return Jwts.parser().verifyWith(Keys.hmacShaKeyFor(jwtSecret.getBytes())).build().parseSignedClaims(token)
+                .getPayload();
+    }
+
+    @Override
+    public boolean isTokenValid(String token) {
+        try {
+            extractClaims(token);
+            return true;
+        } catch (JwtException e) {
+            return false;
+        }
+    }
+}
