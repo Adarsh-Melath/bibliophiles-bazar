608aa2d9a0e154bc112724a524f6b4bb667170aa
Adarsh.M <adarshmelath1305@gmail.com>
2026-05-23 16:14:51 +0530

    refactor(dto): add constructor to LoginResult

--- backend/src/main/java/com/adarsh/backend/feature/auth/application/dto/LoginResult.java
+++ backend/src/main/java/com/adarsh/backend/feature/auth/application/dto/LoginResult.java
@@ -5,27 +5,21 @@ public class LoginResult {
     private String refreshToken;
     private LoginUserResult user;
 
-    public String getAccessToken() {
-        return accessToken;
+    public LoginResult(String accessToken, String refreshToken, LoginUserResult user) {
+        this.accessToken = accessToken;
+        this.refreshToken = refreshToken;
+        this.user = user;
     }
 
-    public void setAccessToken(String accessToken) {
-        this.accessToken = accessToken;
+    public String getAccessToken() {
+        return accessToken;
     }
 
     public String getRefreshToken() {
         return refreshToken;
     }
 
-    public void setRefreshToken(String refreshToken) {
-        this.refreshToken = refreshToken;
-    }
-
     public LoginUserResult getUser() {
         return user;
-    }
-
-    public void setUser(LoginUserResult user) {
-        this.user = user;
     }
 }
