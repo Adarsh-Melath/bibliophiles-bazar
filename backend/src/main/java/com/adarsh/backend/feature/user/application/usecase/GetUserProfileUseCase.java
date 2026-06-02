9a6705da157a32a182fd90ea5ce43bf304c7f33d
adarsh-melath <adarshmelath1305@gmail.com>
2026-06-02 22:14:08 +0530

    feat(user): add email parameter in execute method

--- backend/src/main/java/com/adarsh/backend/feature/user/application/usecase/GetUserProfileUseCase.java
+++ backend/src/main/java/com/adarsh/backend/feature/user/application/usecase/GetUserProfileUseCase.java
@@ -3,5 +3,5 @@ package com.adarsh.backend.feature.user.application.usecase;
 import com.adarsh.backend.feature.user.application.dto.GetUserProfileResult;
 
 public interface GetUserProfileUseCase {
-    GetUserProfileResult execute();
+    GetUserProfileResult execute(String email);
 }
