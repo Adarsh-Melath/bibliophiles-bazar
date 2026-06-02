5de674b78cc4762121f32285cfad86b43fb865fa
adarsh-melath <adarshmelath1305@gmail.com>
2026-06-02 23:49:35 +0530

    feat(user): add getProfile controller

---
+++ backend/src/main/java/com/adarsh/backend/feature/user/presentation/controller/UserController.java
@@ -0,0 +1,27 @@
+package com.adarsh.backend.feature.user.presentation.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.adarsh.backend.feature.user.application.dto.GetUserProfileResult;
+import com.adarsh.backend.feature.user.application.usecase.GetUserProfileUseCase;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@RestController
+@RequestMapping("/api/v1/user")
+@RequiredArgsConstructor
+@Slf4j
+public class UserController{
+    private final GetUserProfileUseCase getUserProfileUseCase;
+
+    @GetMapping("/profile")
+    public ResponseEntity<GetUserProfileResult> getProfile(@AuthenticationPrincipal String email){
+        GetUserProfileResult result=getUserProfileUseCase.execute(email);
+        return ResponseEntity.ok(result);
+    }
+}
