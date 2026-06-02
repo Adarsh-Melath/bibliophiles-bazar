0de31ac49e2d3cf2b01223f3bbd7e656d1d1a807
adarsh-melath <adarshmelath1305@gmail.com>
2026-06-02 22:16:28 +0530

    feat(user): add GetUserProfileInteractor

---
+++ backend/src/main/java/com/adarsh/backend/feature/user/application/interactor/GetUserProfileInteractor.java
@@ -0,0 +1,27 @@
+package com.adarsh.backend.feature.user.application.interactor;
+
+import org.springframework.stereotype.Service;
+
+import com.adarsh.backend.feature.user.application.dto.GetUserProfileResult;
+import com.adarsh.backend.feature.user.application.port.UserCommandRepository;
+import com.adarsh.backend.feature.user.application.usecase.GetUserProfileUseCase;
+import com.adarsh.backend.feature.user.domain.exception.UserNotFoundException;
+import com.adarsh.backend.feature.user.domain.model.User;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class GetUserProfileInteractor implements GetUserProfileUseCase {
+    private final UserCommandRepository userCommandRepository;
+
+    @Override
+    public GetUserProfileResult execute(String email) {
+        User user = userCommandRepository.findByEmail(email)
+                .orElseThrow(() -> new UserNotFoundException("User Not Found"));
+
+        return new GetUserProfilResult(user.getName(), user.getEmail(), user.getRole().name(),
+                user.getProfileImageUrl(), user.getPhone());
+    }
+
+}
