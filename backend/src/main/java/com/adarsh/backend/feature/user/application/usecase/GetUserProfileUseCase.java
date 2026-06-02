98cedd67a560d97c810612f8edb9bc22f75c2ab4
adarsh-melath <adarshmelath1305@gmail.com>
2026-06-02 21:56:12 +0530

    feat(user): add GetUserProfileUseCase

---
+++ backend/src/main/java/com/adarsh/backend/feature/user/application/usecase/GetUserProfileUseCase.java
@@ -0,0 +1,7 @@
+package com.adarsh.backend.feature.user.application.usecase;
+
+import com.adarsh.backend.feature.user.application.dto.GetProfileResult;
+
+public interface GetUserProfileUseCase {
+    GetProfileResult execute();
+}
