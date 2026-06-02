82af73bb862ef0db36dcc22bf79259ae72525ecf
adarsh-melath <adarshmelath1305@gmail.com>
2026-06-03 00:04:35 +0530

    feat(user): add ChangePasswordUseCase

---
+++ backend/src/main/java/com/adarsh/backend/feature/user/application/usecase/ChangePasswordUseCase.java
@@ -0,0 +1,7 @@
+package com.adarsh.backend.feature.user.application.usecase;
+
+import com.adarsh.backend.feature.user.application.dto.ChangePasswordCommand;
+
+public interface ChangePasswordUseCase {
+    void execute(ChangePasswordCommand command);
+}
