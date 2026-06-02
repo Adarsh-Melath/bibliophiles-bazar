40b8bc8eb5f2324debe66a9f7537e90e7234bf7c
adarsh-melath <adarshmelath1305@gmail.com>
2026-06-02 22:06:38 +0530

    feat(user): add GetUserProfileResult

--- backend/src/main/java/com/adarsh/backend/feature/user/application/dto/GetUserProfileResult.java
+++ backend/src/main/java/com/adarsh/backend/feature/user/application/dto/GetUserProfileResult.java
@@ -1,13 +1,13 @@
 package com.adarsh.backend.feature.user.application.dto;
 
-public class GetProfileResult {
+public class GetUserProfileResult {
     private String name;
     private String email;
     private String role;
     private String profileImageUrl;
     private String phone;
 
-    public GetProfileResult(String name, String email, String role, String profileImageUrl, String phone) {
+    public GetUserProfileResult(String name, String email, String role, String profileImageUrl, String phone) {
         this.name = name;
         this.email = email;
         this.role = role;
