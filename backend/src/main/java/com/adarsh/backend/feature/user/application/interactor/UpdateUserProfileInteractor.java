320c5d086aef3f495fadd1549f80fa3e067986ef
adarsh-melath <adarshmelath1305@gmail.com>
2026-06-03 19:08:58 +0530

    feat(user): add UpdateUserProfileInteractor

---
+++ backend/src/main/java/com/adarsh/backend/feature/user/application/interactor/UpdateUserProfileInteractor.java
@@ -0,0 +1,29 @@
+package com.adarsh.backend.feature.user.application.interactor;
+
+import org.springframework.stereotype.Service;
+
+import com.adarsh.backend.feature.user.application.dto.UpdateUserProfileCommand;
+import com.adarsh.backend.feature.user.application.dto.UpdateUserProfileResult;
+import com.adarsh.backend.feature.user.application.port.UserCommandRepository;
+import com.adarsh.backend.feature.user.domain.exception.UserNotFoundException;
+
+import com.adarsh.backend.feature.user.domain.model.User;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class UpdateUserProfileInteractor implements UpdateUserProfileUseCase {
+
+    private final UserCommandRepository userCommandRepository;
+
+    @Override
+    public UpdateUserProfileResult execute(String email, UpdateUserProfileCommand command) {
+        User user = userCommandRepository.findByEmail(email).orElseThrow(() -> new UserNotFoundException("User Not Found "));
+
+        user.updateProfile(command.getName(), command.getPhone(), command.getProfileImage());
+        userCommandRepository.save(user);
+        return new UpdateUserProfileResult(user.getName(), user.getPhone(), user.getProfileImageUrl());
+    }
+
+}
