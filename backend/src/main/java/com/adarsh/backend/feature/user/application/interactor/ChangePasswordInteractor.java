50f8411e36d79f8d9ef330febaa00f19efc4fcea
adarsh-melath <adarshmelath1305@gmail.com>
2026-06-03 11:15:59 +0530

    feat(user): add ChangePasswordInteractor

---
+++ backend/src/main/java/com/adarsh/backend/feature/user/application/interactor/ChangePasswordInteractor.java
@@ -0,0 +1,32 @@
+package com.adarsh.backend.feature.user.application.interactor;
+
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import com.adarsh.backend.feature.user.application.dto.ChangePasswordCommand;
+import com.adarsh.backend.feature.user.application.port.UserCommandRepository;
+import com.adarsh.backend.feature.user.application.usecase.ChangePasswordUseCase;
+import com.adarsh.backend.feature.user.domain.exception.PasswordIncorrectException;
+import com.adarsh.backend.feature.user.domain.exception.UserNotFoundException;
+import com.adarsh.backend.feature.user.domain.model.User;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class ChangePasswordInteractor implements ChangePasswordUseCase {
+    private final PasswordEncoder passwordEncoder;
+    private final UserCommandRepository userCommandRepository;
+
+    @Override
+    public void execute(String email,ChangePasswordCommand command){
+        User user=userCommandRepository.findByEmail(email).orElseThrow(()->new UserNotFoundException("User Not Found"));
+
+        if(!passwordEncoder.matches(user.getPassword(),command.getCurrentPassword())){
+            throw new PasswordIncorrectException("Current Password is incorrect");
+        }
+
+        user.changePassword(passwordEncoder.encode(command.getNewPassword()));
+        userCommandRepository.save(user);
+    }
+}
