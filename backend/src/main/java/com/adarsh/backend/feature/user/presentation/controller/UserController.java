f0e9dc880f695e0a7d66be514298fdc36a784c99
adarsh-melath <adarshmelath1305@gmail.com>
2026-06-03 11:57:12 +0530

    feat(user): add changePassword controller

--- backend/src/main/java/com/adarsh/backend/feature/user/presentation/controller/UserController.java
+++ backend/src/main/java/com/adarsh/backend/feature/user/presentation/controller/UserController.java
@@ -3,10 +3,14 @@ package com.adarsh.backend.feature.user.presentation.controller;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.adarsh.backend.feature.user.application.dto.ChangePasswordCommand;
 import com.adarsh.backend.feature.user.application.dto.GetUserProfileResult;
+import com.adarsh.backend.feature.user.application.usecase.ChangePasswordUseCase;
 import com.adarsh.backend.feature.user.application.usecase.GetUserProfileUseCase;
 
 import lombok.RequiredArgsConstructor;
@@ -18,10 +22,17 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class UserController{
     private final GetUserProfileUseCase getUserProfileUseCase;
+    private final ChangePasswordUseCase changePasswordUseCase;
 
     @GetMapping("/profile")
     public ResponseEntity<GetUserProfileResult> getProfile(@AuthenticationPrincipal String email){
         GetUserProfileResult result=getUserProfileUseCase.execute(email);
         return ResponseEntity.ok(result);
+    }
+
+    @PostMapping("/change-password")
+    public ResponseEntity<String> changePassword(@AuthenticationPrincipal String email, @RequestBody ChangePasswordCommand command){
+        changePasswordUseCase.execute(email, command);
+        return ResponseEntity.ok("Password Changed Successfully");
     }
 }
