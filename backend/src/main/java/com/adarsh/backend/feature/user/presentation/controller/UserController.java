57ec19e4fcd8c089bd42f89599160b58a6f22d98
adarsh-melath <adarshmelath1305@gmail.com>
2026-06-03 19:53:11 +0530

    feat(user): add updateProfile controller

--- backend/src/main/java/com/adarsh/backend/feature/user/presentation/controller/UserController.java
+++ backend/src/main/java/com/adarsh/backend/feature/user/presentation/controller/UserController.java
@@ -12,6 +12,9 @@ import com.adarsh.backend.feature.user.application.dto.ChangePasswordCommand;
 import com.adarsh.backend.feature.user.application.dto.GetUserProfileResult;
 import com.adarsh.backend.feature.user.application.usecase.ChangePasswordUseCase;
 import com.adarsh.backend.feature.user.application.usecase.GetUserProfileUseCase;
+import com.adarsh.backend.feature.user.application.dto.UpdateUserProfileCommand;
+import com.adarsh.backend.feature.user.application.dto.UpdateUserProfileResult;
+import com.adarsh.backend.feature.user.application.usecase.UpdateUserProfileUseCase;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -20,18 +23,26 @@ import lombok.extern.slf4j.Slf4j;
 @RequestMapping("/api/v1/user")
 @RequiredArgsConstructor
 @Slf4j
-public class UserController{
+public class UserController {
+
     private final GetUserProfileUseCase getUserProfileUseCase;
     private final ChangePasswordUseCase changePasswordUseCase;
+    private final UpdateUserProfileUseCase updateUserProfileUseCase;
 
     @GetMapping("/profile")
-    public ResponseEntity<GetUserProfileResult> getProfile(@AuthenticationPrincipal String email){
-        GetUserProfileResult result=getUserProfileUseCase.execute(email);
+    public ResponseEntity<GetUserProfileResult> getProfile(@AuthenticationPrincipal String email) {
+        GetUserProfileResult result = getUserProfileUseCase.execute(email);
+        return ResponseEntity.ok(result);
+    }
+
+    @PostMapping("/update-profile")
+    public ResponseEntity<UpdateUserProfileResult> updateProfile(@AuthenticationPrincipal String email,  @RequestBody UpdateUserProfileCommand command){
+        UpdateUserProfileResult result = updateUserProfileUseCase.execute(email, command);
         return ResponseEntity.ok(result);
     }
 
     @PostMapping("/change-password")
-    public ResponseEntity<String> changePassword(@AuthenticationPrincipal String email, @RequestBody ChangePasswordCommand command){
+    public ResponseEntity<String> changePassword(@AuthenticationPrincipal String email, @RequestBody ChangePasswordCommand command) {
         changePasswordUseCase.execute(email, command);
         return ResponseEntity.ok("Password Changed Successfully");
     }
