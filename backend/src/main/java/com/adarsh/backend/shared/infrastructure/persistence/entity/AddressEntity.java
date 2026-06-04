3d6523829ba05fa76220485511a933f02e3d76c9
adarsh-melath <adarshmelath1305@gmail.com>
2026-06-04 16:31:47 +0530

    feat(shared): add AddressEntity

---
+++ backend/src/main/java/com/adarsh/backend/shared/infrastructure/persistence/entity/AddressEntity.java
@@ -0,0 +1,53 @@
+package com.adarsh.backend.shared.infrastructure.persistence.entity;
+
+import com.adarsh.backend.feature.user.infrastructure.persistence.entity.UserEntity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name="addresses")
+@Getter
+@Setter
+public class AddressEntity {
+       @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private UserEntity user;
+
+    @Column(nullable = false)
+    private String fullName;
+
+    @Column(nullable = false)
+    private String phone;
+
+    @Column(nullable = false)
+    private String addressLine;
+
+    @Column(nullable = false)
+    private String city;
+
+    @Column(nullable = false)
+    private String state;
+
+    @Column(nullable = false)
+    private String pincode;
+
+    private boolean isDefault = false;
+
+    private String addressLine2;
+    private String country;
+    private String addressType;
+}
