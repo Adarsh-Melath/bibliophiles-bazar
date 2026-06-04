53255dd56dfee335c04bc0672f035482b8467921
adarsh-melath <adarshmelath1305@gmail.com>
2026-06-04 20:12:57 +0530

    feat(user): change GetAddressResult import statement

--- backend/src/main/java/com/adarsh/backend/feature/user/application/usecase/GetAddressesUseCase.java
+++ backend/src/main/java/com/adarsh/backend/feature/user/application/usecase/GetAddressesUseCase.java
@@ -2,7 +2,7 @@ package com.adarsh.backend.feature.user.application.usecase;
 
 import java.util.List;
 
-import com.adarsh.backend.shared.application.dto.GetAddressResult;
+import com.adarsh.backend.feature.user.application.dto.GetAddressResult;
 
 public interface GetAddressesUseCase {
     List<GetAddressResult> execute(String email);
