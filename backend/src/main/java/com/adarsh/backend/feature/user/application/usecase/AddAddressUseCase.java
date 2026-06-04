cb3913e270952577eda494e6f4feaf29a5b33ec9
adarsh-melath <adarshmelath1305@gmail.com>
2026-06-04 18:39:55 +0530

    refactor(user): add email parameter

--- backend/src/main/java/com/adarsh/backend/feature/user/application/usecase/AddAddressUseCase.java
+++ backend/src/main/java/com/adarsh/backend/feature/user/application/usecase/AddAddressUseCase.java
@@ -3,5 +3,5 @@ package com.adarsh.backend.feature.user.application.usecase;
 import com.adarsh.backend.feature.user.application.dto.AddAddressResult;
 import com.adarsh.backend.feature.user.application.dto.AddAddressCommand;
 public interface AddAddressUseCase {
-    AddAddressResult execute(AddAddressCommand command);
+    AddAddressResult execute(String email,AddAddressCommand command);
 }
