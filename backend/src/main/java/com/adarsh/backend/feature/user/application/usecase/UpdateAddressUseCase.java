2d87dbb8670589986f4214e93eaea1d508df8d2f
adarsh-melath <adarshmelath1305@gmail.com>
2026-06-04 20:24:16 +0530

    refactor(user): change UpdateAddressCommand import statement

--- backend/src/main/java/com/adarsh/backend/feature/user/application/usecase/UpdateAddressUseCase.java
+++ backend/src/main/java/com/adarsh/backend/feature/user/application/usecase/UpdateAddressUseCase.java
@@ -1,8 +1,7 @@
 package com.adarsh.backend.feature.user.application.usecase;
 
+import com.adarsh.backend.feature.user.application.dto.UpdateAddressCommand;
 import com.adarsh.backend.feature.user.application.dto.UpdateAddressResult;
-import com.adarsh.backend.feature.user.application.dto.command.UpdateAddressCommand;
-
 public interface UpdateAddressUseCase {
     UpdateAddressResult   execute(String email, Long id, UpdateAddressCommand command);
 }
