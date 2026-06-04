c441385d12efc2a36f9f71c564ebac715e469d68
adarsh-melath <adarshmelath1305@gmail.com>
2026-06-04 18:40:56 +0530

    feat(user): add AddAddressInteractor

---
+++ backend/src/main/java/com/adarsh/backend/feature/user/application/interactor/AddAddressInteractor.java
@@ -0,0 +1,54 @@
+package com.adarsh.backend.feature.user.application.interactor;
+
+import org.springframework.stereotype.Service;
+import com.adarsh.backend.feature.user.application.usecase.AddAddressUseCase;
+import lombok.RequiredArgsConstructor;
+import com.adarsh.backend.feature.user.application.port.UserCommandRepository;
+import com.adarsh.backend.feature.user.application.port.AddressRepositoryPort;
+import com.adarsh.backend.feature.user.domain.User;
+import com.adarsh.backend.feature.user.application.dto.AddAddressCommand;
+import com.adarsh.backend.feature.user.application.dto.AddAddressResult;
+import com.adarsh.backend.feature.user.domain.exception.UserNotFoundException;
+import com.adarsh.backend.feature.user.domain.model.Address;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class AddAddressInteractor implements AddAddressUseCase {
+
+    private final UserCommandRepository userCommandRepository;
+    private final AddressRepositoryPort addressRepositoryPort;
+
+    @Override
+    public AddAddressResult execute(String email, AddAddressCommand command) {
+        com.adarsh.backend.feature.user.domain.model.User user = userCommandRepository.findByEmail(email).orElseThrow(() -> new UserNotFoundException("User not found with email: " + email));
+
+        if (command.isDefault()) {
+            List<Address> addresses = addressRepositoryPort.findByUserId(user.getId());
+
+            for (Address address : addresses) {
+                address.markAsNonDefault();
+                addressRepositoryPort.save(address);
+            }
+        }
+
+        Address newAddress=new Address.Builder()
+        .userId(user.getId())
+        .fullName(command.getFullName())
+        .phone(command.getPhone())
+        .addressLine(command.getAddressLine())
+        .city(command.getCity())
+        .state(command.getState())
+        .pincode(command.getPincode())
+        .isDefault(command.isDefault())
+        .addressLine2(command.getAddressLine2())
+        .country(command.getCountry())
+        .addressType(command.getAddressType())
+        .build();
+
+        addressRepositoryPort.save(newAddress);
+
+        return new AddAddressResult(newAddress.getId(),newAddress.getFullName(),newAddress.getPhone(),newAddress.getAddressLine(),newAddress.getCity(),newAddress.getState(),newAddress.getPincode(),newAddress.isDefault(),newAddress.getAddressLine2(),newAddress.getCountry(),newAddress.getAddressType());
+    }
+}
+
