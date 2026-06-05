61bab51ecdba7ee77d47c6c649d14d4511105c71
adarsh-melath <adarshmelath1305@gmail.com>
2026-06-05 10:30:49 +0530

    refactor(admin): fix parameter type of enabled

--- backend/src/main/java/com/adarsh/backend/feature/admin/application/usecase/GetUserUseCase.java
+++ backend/src/main/java/com/adarsh/backend/feature/admin/application/usecase/GetUserUseCase.java
@@ -1,8 +1,9 @@
 package com.adarsh.backend.feature.admin.application.usecase;
 
 import com.adarsh.backend.feature.admin.application.dto.GetUserResult;
+import com.adarsh.backend.feature.user.domain.model.Role;
 import com.adarsh.backend.feature.user.domain.pagination.PageResult;
 
 public interface GetUserUseCase {
-    PageResult<GetUserResult> execute(int page,int size);
+    PageResult<GetUserResult> execute(String keyword,Role role,Boolean enabled,int page,int size);
 }
