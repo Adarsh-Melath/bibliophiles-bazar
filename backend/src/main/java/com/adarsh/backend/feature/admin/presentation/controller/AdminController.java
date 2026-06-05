43b4a43869c92b9949f7f85bc50fe7be51404fe4
adarsh-melath <adarshmelath1305@gmail.com>
2026-06-05 10:29:40 +0530

    feat(admin): add getUsers controller

--- backend/src/main/java/com/adarsh/backend/feature/admin/presentation/controller/AdminController.java
+++ backend/src/main/java/com/adarsh/backend/feature/admin/presentation/controller/AdminController.java
@@ -1,11 +1,19 @@
 package com.adarsh.backend.feature.admin.presentation.controller;
+
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.adarsh.backend.feature.admin.application.dto.GetUserResult;
+import com.adarsh.backend.feature.admin.application.usecase.GetUserUseCase;
 import com.adarsh.backend.feature.admin.application.usecase.ToggleBlockUseCase;
+import com.adarsh.backend.feature.user.domain.model.Role;
+import com.adarsh.backend.feature.user.domain.pagination.PageResult;
 
 import lombok.RequiredArgsConstructor;
 
@@ -13,10 +21,26 @@ import lombok.RequiredArgsConstructor;
 @RequestMapping("/api/v1/admin")
 @RequiredArgsConstructor
 public class AdminController {
-    private final ToggleBlockUseCase toggleBlockUseCase;
 
-    @PostMapping("/users/{id}/toggle-block")
-    public ResponseEntity<String> toggleBlockUser(@PathVariable Long id){
+    private final ToggleBlockUseCase toggleBlockUseCase;
+    private final GetUserUseCase getUserUseCase;
+
+    @GetMapping("/users")
+    @PreAuthorize("hasRole('ADMIN')")
+    public ResponseEntity<PageResult<GetUserResult>> getUsers(
+            @RequestParam(required = false) String search,
+            @RequestParam(required = false) Role role,
+            @RequestParam(required = false) Boolean enabled,
+            @RequestParam(defaultValue = "0") int page,
+            @RequestParam(defaultValue = "10") int size
+    ) {
+        PageResult<GetUserResult> result = getUserUseCase.execute(search, role, enabled, page, size);
+        return ResponseEntity.ok(result);
+    }
+
+    @PutMapping("/users/{id}/toggle-block")
+    @PreAuthorize("hasRole('ADMIN')")
+    public ResponseEntity<String> toggleBlockUser(@PathVariable Long id) {
         toggleBlockUseCase.execute(id);
         return ResponseEntity.ok("User block status toggled successfully");
     }
