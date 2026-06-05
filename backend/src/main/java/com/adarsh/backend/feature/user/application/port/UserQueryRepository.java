d7c14811ec1e862cdb53e1ccae3eed0fc5e35a4e
adarsh-melath <adarshmelath1305@gmail.com>
2026-06-05 10:35:23 +0530

    refactor(admin): remove unused method

--- backend/src/main/java/com/adarsh/backend/feature/user/application/port/UserQueryRepository.java
+++ backend/src/main/java/com/adarsh/backend/feature/user/application/port/UserQueryRepository.java
@@ -1,14 +1,11 @@
 package com.adarsh.backend.feature.user.application.port;
 
-// Used by Admin/Dashboard Use Cases
 
 import com.adarsh.backend.feature.user.domain.model.User;
 import com.adarsh.backend.feature.user.domain.pagination.PageQuery;
 import com.adarsh.backend.feature.user.domain.pagination.PageResult;
 
+    
 public interface UserQueryRepository {
-    PageResult<User> findAll(PageQuery query);
-
-
     PageResult<User> search(UserSearchCriteria criteria, PageQuery pageable);
 }
