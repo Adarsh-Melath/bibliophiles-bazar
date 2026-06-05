f61eb9aad5bf59825fb904e2079897c7443cc009
adarsh-melath <adarshmelath1305@gmail.com>
2026-06-05 20:53:43 +0530

    feat(vendor): add ApplicationNotFoundException handler

--- backend/src/main/java/com/adarsh/backend/feature/vendor/infrastructure/web/VendorExceptionHandler.java
+++ backend/src/main/java/com/adarsh/backend/feature/vendor/infrastructure/web/VendorExceptionHandler.java
@@ -8,21 +8,33 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import com.adarsh.backend.feature.vendor.domain.exception.ApplicationAlreadyExistException;
+import com.adarsh.backend.feature.vendor.domain.exception.ApplicationNotFoundException;
 
 import jakarta.servlet.http.HttpServletRequest;
 
 @RestControllerAdvice
 public class VendorExceptionHandler {
+    @ExceptionHandler(ApplicationNotFoundException.class)
+    public ResponseEntity<ErrorResponse> handleApplicationNotFoundException(ApplicationNotFoundException ex,
+            HttpServletRequest request) {
+        ErrorResponse errorResponse = new ErrorResponse(
+                LocalDateTime.now(),
+                HttpStatus.NOT_FOUND.value(),
+                "Not Found",
+                ex.getMessage(),
+                request.getRequestURI());
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(errorResponse);
+    }
+
     @ExceptionHandler(ApplicationAlreadyExistException.class)
     public ResponseEntity<ErrorResponse> handleApplicationAlreadyExistException(ApplicationAlreadyExistException ex,
             HttpServletRequest request) {
-            ErrorResponse errorResponse=new ErrorResponse(
+        ErrorResponse errorResponse = new ErrorResponse(
                 LocalDateTime.now(),
                 HttpStatus.CONFLICT.value(),
                 "Already Exists",
                 ex.getMessage(),
-                request.getRequestURI()
-            );
+                request.getRequestURI());
 
         return ResponseEntity.status(HttpStatus.CONFLICT).body(errorResponse);
     }
