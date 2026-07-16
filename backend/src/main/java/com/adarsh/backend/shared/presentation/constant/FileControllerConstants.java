1c6c2e0ad49c26f60fb71128df6d28362161e5ec
Adarsh.M <adarshmelath1305@gmail.com>
2026-07-16 19:55:52 +0530

    fix(shared): include query parameters in request URI

--- backend/src/main/java/com/adarsh/backend/shared/presentation/constant/FileControllerConstants.java
+++ backend/src/main/java/com/adarsh/backend/shared/presentation/constant/FileControllerConstants.java
@@ -6,7 +6,7 @@ public final class FileControllerConstants {
     public static final String BASE_PATH = "/api/v1/media";
 
     // Endpoints
-    public static final String UPLOAD_URL_PATH = "/upload-url";
+    public static final String UPLOAD_URL_PATH = "/upload-url/{fileName}/{contentType}";
     public static final String UPLOAD_URLS_PATH = "/upload-urls";
     public static final String DOWNLOAD_URL_PATH = "/download-url";
 
